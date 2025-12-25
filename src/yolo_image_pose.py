9be7b184729fa595e5afaca46b9156d97991d3ce
Alejandro Rey <alereyleyva@gmail.com>
2025-12-25 18:27:52 +0100

    feat: implement image pose with YOLO

---
+++ src/yolo_image_pose.py
@@ -0,0 +1,18 @@
+import sys
+import torch
+from ultralytics import YOLO
+
+model = YOLO(model="models/yolo_pose/yolo11l-pose.pt")
+
+device = torch.device("mps" if torch.mps.is_available() else "cpu")
+
+if len(sys.argv) < 2:
+    raise ValueError("Usage: python image_pose.py image_path")
+
+image_path = sys.argv[1]
+
+results = model.track(source=image_path, conf=0.7, device=device)
+
+result = results[0]
+
+result.show()
